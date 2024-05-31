301f25536e0ea60fef15bac334390dd4bc245a49
kruthirhegde <kruthirhegde@gmail.com>
2024-05-31 12:31:22 +0530

    first push

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+     <center> <h style={{fontsize:'30px'}}>Fashions Pro</h></center>
+    <center>  <h1><img src="https://images.unsplash.com/photo-1612547036242-77002603e5aa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTN8fHxlbnwwfHx8fHw%3D" style={{width:'600px',height:'600px',padding:'100px',backgroundColor:'burlywood'}}></img></h1></center>
+    </div>
+  );
+}
+
+export default Home;
